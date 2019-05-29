c6da21ec82624a2714acd45ceefcda87771adb0f
Eddie James <eajames@linux.ibm.com>
2019-05-29 18:29:58 +0000

    Change AspeedXdmaOp structure to match latest kernel
    This structure changed in the driver as part of the upstreaming process. There
    shouldn't be any further changes.
    
    Signed-off-by: Eddie James <eajames@linux.ibm.com>
    Change-Id: I24fe323d2c5bf1e3463304f067441982d55d0766

--- libpldmresponder/file_io.cpp
+++ libpldmresponder/file_io.cpp
@@ -30,13 +30,13 @@ namespace dma
  */
 struct AspeedXdmaOp
 {
-    uint8_t upstream;  //!< boolean indicating the direction of the DMA
-                       //!< operation, true means a transfer from BMC to host.
     uint64_t hostAddr; //!< the DMA address on the host side, configured by
                        //!< PCI subsystem.
     uint32_t len;      //!< the size of the transfer in bytes, it should be a
                        //!< multiple of 16 bytes
-} __attribute__((packed));
+    uint32_t upstream; //!< boolean indicating the direction of the DMA
+                       //!< operation, true means a transfer from BMC to host.
+};
 
 constexpr auto xdmaDev = "/dev/xdma";
 
